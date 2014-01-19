82f5431c80cdc6ca3f22ccdd1a01154a8e23f683
Jonatron <jonath4n@gmail.com>
2014-01-19 00:21:19 +0000

    make clearable if blank=True add setting ADMIN_RESUMABLE_CHUNKSIZE

--- admin_resumable/fields.py
+++ admin_resumable/fields.py
@@ -4,16 +4,35 @@ from django.db import models
 from django.templatetags.static import static
 from django.template import loader
 from django.forms.fields import FileField
+from django.forms.widgets import CheckboxInput
 from django.core.exceptions import ValidationError
+from django.conf import settings
+from django.utils.translation import ugettext_lazy
+from django.utils.safestring import mark_safe
 
 class ResumableWidget(FileInput):
     template_name = 'admin_resumable/file_input.html'
+    clear_checkbox_label = ugettext_lazy('Clear')
 
     def render(self, name, value, attrs=None, **kwargs):
-        context = {'name': name, 'value': value, 'id': attrs['id']}
+        chunkSize = getattr(settings, 'ADMIN_RESUMABLE_CHUNKSIZE', "1*1024*1024")
+        context = {'name': name, 'value': value, 'id': attrs['id'], 'chunkSize': chunkSize}
+        if not self.is_required:
+            template_with_clear = '<span class="clearable-file-input">%(clear)s <label for="%(clear_checkbox_id)s">%(clear_checkbox_label)s</label></span>'
+            substitutions = {}
+            substitutions['clear_checkbox_id'] = attrs['id'] + "-clear-id"
+            substitutions['clear_checkbox_name'] = attrs['id'] + "-clear"
+            substitutions['clear_checkbox_label'] = self.clear_checkbox_label
+            substitutions['clear'] = CheckboxInput().render(substitutions['clear_checkbox_name'],
+                False, attrs={'id': substitutions['clear_checkbox_id']})
+            clear_checkbox = mark_safe(template_with_clear % substitutions)
+            
+            context.update({'clear_checkbox': clear_checkbox})
         return loader.render_to_string(self.template_name, context)
 
     def value_from_datadict(self, data, files, name):
+        if not self.is_required and data.get("id_" + name + "-clear"):
+            return False # False signals to clear any existing value, as opposed to just None
         return data.get(name, None)
 
 
