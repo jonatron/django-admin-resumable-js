a5061ea611a9a2604531485ad59a7820ab66357b
Florian Demmer <fdemmer@gmail.com>
2017-04-04 15:39:35 +0200

    Update formatting

--- admin_resumable/views.py
+++ admin_resumable/views.py
@@ -23,9 +23,10 @@ def get_chunks_subdir():
 def get_chunks_dir():
     chunks_subdir = get_chunks_subdir()
     media_root = getattr(settings, 'MEDIA_ROOT', None)
+
     if not media_root:
-        raise ImproperlyConfigured(
-            'You must set settings.MEDIA_ROOT')
+        raise ImproperlyConfigured('You must set settings.MEDIA_ROOT')
+
     chunks_dir = os.path.join(media_root, chunks_subdir)
     ensure_dir(chunks_dir)
     return chunks_dir
@@ -76,6 +77,7 @@ def get_upload_to(request):
 def admin_resumable(request):
     upload_to = get_upload_to(request)
     storage = get_storage(upload_to)
+
     if request.method == 'POST':
         chunk = request.FILES.get('file')
         r = ResumableFile(storage, request.POST)
@@ -86,6 +88,7 @@ def admin_resumable(request):
             r.delete_chunks()
             return HttpResponse(storage.url(actual_filename))
         return HttpResponse('chunk uploaded')
+
     elif request.method == 'GET':
         r = ResumableFile(storage, request.GET)
         if not r.chunk_exists:
