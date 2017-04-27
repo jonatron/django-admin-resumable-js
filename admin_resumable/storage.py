fd058794443b059086bfb6e71658dfd28ea8702c
Florian Demmer <fdemmer@gmail.com>
2017-04-27 20:09:18 +0200

    Add ResumableDefaultStorage as simple wrapper around ADMIN_RESUMABLE_STORAGE

--- admin_resumable/storage.py
+++ admin_resumable/storage.py
@@ -1,28 +1,13 @@
-from django.conf import settings
+# -*- coding: utf-8 -*-
 from django.core.files.storage import get_storage_class
+from django.utils.functional import LazyObject
 
-from admin_resumable.conf import ADMIN_RESUMABLE_STORAGE
+from admin_resumable import conf
 
 
-def get_storage():
-    """
-    Looks at the ADMIN_RESUMABLE_STORAGE setting and returns
-    an instance of the storage class specified.
+class ResumableDefaultStorage(LazyObject):
+    def _setup(self):
+        self._wrapped = get_storage_class(conf.ADMIN_RESUMABLE_STORAGE)()
 
-    Defaults to django.core.files.storage.FileSystemStorage.
 
-    Any custom storage class used here must either be a subclass of
-    django.core.files.storage.FileSystemStorage, or accept a location
-    init parameter.
-    """
-    base_url = settings.MEDIA_URL
-    location = settings.MEDIA_ROOT
-
-    #subdir = getattr(settings, 'ADMIN_RESUMABLE_SUBDIR', None)
-    #if subdir is not None:
-    #    base_url = settings.MEDIA_URL + subdir
-    #    location = os.path.join(settings.MEDIA_ROOT, subdir)
-    #    ensure_dir(location)
-
-    storage_class = get_storage_class(ADMIN_RESUMABLE_STORAGE)
-    return storage_class(location=location, base_url=base_url)
+resumable_default_storage = ResumableDefaultStorage()
