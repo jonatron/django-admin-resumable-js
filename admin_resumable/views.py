4e323a7d56c32e9c383e31445fe37efb41bf8781
Florian Demmer <fdemmer@gmail.com>
2017-04-27 20:07:09 +0200

    Rewrite view as cbv

--- admin_resumable/views.py
+++ admin_resumable/views.py
@@ -1,43 +1,82 @@
-from __future__ import absolute_import
-
+# -*- coding: utf-8 -*-
+import logging
 
 from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.contenttypes.models import ContentType
 from django.http import HttpResponse
+from django.utils.decorators import method_decorator
+from django.utils.functional import cached_property
+from django.views import View
 
-from admin_resumable.storage import get_storage
-from admin_resumable.utils import get_model_field
-from .files import ResumableFile
+from admin_resumable.files import ResumableFile
+from admin_resumable.storage import resumable_default_storage
+
+log = logging.getLogger(__name__)
 
 
+@method_decorator(staff_member_required, name='dispatch')
+class ResumableView(View):
+    @cached_property
+    def params(self):
+        return getattr(self.request, self.request.method)
 
-@staff_member_required
-def admin_resumable(request):
-    field = get_model_field(request)
-    storage = get_storage()
+    @cached_property
+    def model_field(self):
+        """
+        Determine the model field for the uploaded file/chunk using the
+        'content_type_id' and 'field_name' request parameters.
+        """
+        ctype = ContentType.objects.get_for_id(self.params['content_type_id'])
+        # noinspection PyProtectedMember
+        return ctype.model_class()._meta.get_field(self.params['field_name'])
 
-    if request.method == 'POST':
-        chunk = request.FILES.get('file')
-        r = ResumableFile(storage, request.POST)
-        if not r.chunk_exists:
-            r.process_chunk(chunk)
-        if r.is_complete:
-            actual_filename = storage.save(
-                field.generate_filename(None, r.filename),
-                r.file, max_length=field.max_length
-            )
-            r.delete_chunks()
-            return HttpResponse(actual_filename)
+    @cached_property
+    def storage(self):
+        return resumable_default_storage
+
+    @cached_property
+    def resumable(self):
+        return ResumableFile(self.storage, self.params)
+
+    def post(self, request, *args, **kwargs):
+        if not self.resumable.chunk_exists:
+            chunk = request.FILES.get('file')
+            self.resumable.process_chunk(chunk)
+
+        if self.resumable.is_complete:
+            log.debug('POST: is complete')
+            return self.file_is_complete()
+
         return HttpResponse('chunk uploaded')
 
-    elif request.method == 'GET':
-        r = ResumableFile(storage, request.GET)
-        if not r.chunk_exists:
+    def get(self, request, *args, **kwargs):
+        if not self.resumable.chunk_exists:
             return HttpResponse('chunk not found', status=404)
-        if r.is_complete:
-            actual_filename = storage.save(
-                field.generate_filename(None, r.filename),
-                r.file, max_length=field.max_length
-            )
-            r.delete_chunks()
-            return HttpResponse(actual_filename)
+
+        if self.resumable.is_complete:
+            log.debug('GET: is complete')
+            return self.file_is_complete()
+
         return HttpResponse('chunk exists')
+
+    def file_is_complete(self, delete_chunks=True):
+        # get filename incl upload_to and truncating to max_length
+        filename = self.model_field.generate_filename(
+            None, self.resumable.filename
+        )
+
+        log.debug('saving file...')
+        actual_filename = self.storage.save(
+            filename,
+            self.resumable.file,
+            max_length=self.model_field.max_length
+        )
+        log.debug('file saved (%s)', actual_filename)
+
+        if delete_chunks:
+            self.resumable.delete_chunks()
+            log.debug('deleted chunks')
+
+        return HttpResponse(actual_filename)
+
+admin_resumable = ResumableView.as_view()
