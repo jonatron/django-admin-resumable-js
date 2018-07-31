0f4c31da187a4834ffa1edf4663b6ebc40bbb89c
Jonatron <jonathan.aylard@ostmodern.co.uk>
2018-07-31 16:03:51 +0100

    travis?!

--- README.rst
+++ README.rst
@@ -66,7 +66,7 @@ python 3.2 and 3.3 supported up to django 1.8.
 {py27,py34,py35}-django{1.8,1.9,1.10,1.11}
 
 3.0:
-{py27,py35,py36,py37}-django{1.8,1.11,2.0}
+{py27,py35,py36,py37}-django{1.11,2.0}
 
 
 Running Tests Locally
--- tests/urls.py
+++ tests/urls.py
@@ -1,19 +1,19 @@
 import django
 from django.conf.urls import include, url
-from django.urls import path
+#from django.urls import path
 from django.contrib import admin
 from django.conf import settings
 
 
 urlpatterns = [
     url(r'^admin_resumable/', include('admin_resumable.urls')),
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
 ]
 
 
 if settings.DEBUG:
     # static files (images, css, javascript, etc.)
     urlpatterns += [
-        path(r'^media/(?P<path>.*)$', django.views.static.serve,
-             kwargs={'document_root': settings.MEDIA_ROOT})
+        (r'^media/(?P<path>.*)$', django.views.static.serve,
+            {'document_root': settings.MEDIA_ROOT})
     ]
